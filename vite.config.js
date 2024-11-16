afb10c89a570da2b7c8e11e94f7b170b846ab17d
vcb88 <129554845+vcb88@users.noreply.github.com>
2024-11-16 00:18:37 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/compound-interest-calculator/',
+  build: {
+    outDir: 'dist'
+  }
+})
